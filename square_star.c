322fe505067ef9bd7d982194c9da022b13f8106a
Lakshmithra <lakshmithraramesh2007@gmail.com>
2025-06-12 18:53:45 +0530

    Update square_star.c

--- square_star.c
+++ square_star.c
@@ -1,13 +1,19 @@
 // printing square of stars
 
 #include <stdio.h>
-
 int main(){
-   int i,j;
-   for(i=1;i<=5;i++){
-    for(j=1;j<=5;j++){
-    printf("%c",'*');
+   int i,j,rows,columns;
+
+   printf("Enter number of rows:");
+   scanf("%d",&rows);
+
+   printf("Enter number of columns:");
+   scanf("%d",&columns);
+
+    for(i=1;i<=columns;i++){
+      for(j=1;j<=rows;j++){
+      printf("%c",'*');
     }
-        printf("%c\n",'*');
+    printf("%c\n",'*');
    }
 }
